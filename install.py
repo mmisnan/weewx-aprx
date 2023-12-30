b85ef0d59029490c3905e63f0532c770fa43a767
mmisnan <mmisnan@gmail.com>
2023-12-30 16:34:43 +0800

    Update install.py

--- install.py
+++ install.py
@@ -1,5 +1,4 @@
-# installer for cwxn
-# Copyright 2014-2020 Matthew Wall
+# installer for weewx-aprx
 # Distributed under the terms of the GNU Public License (GPLv3)
 
 from weecfg.extension import ExtensionInstaller
@@ -10,14 +9,14 @@ def loader():
 class weewxaprxInstaller(ExtensionInstaller):
     def __init__(self):
         super(weewxaprxInstaller, self).__init__(
-            version="0.5",
+            version="0.1",
             name='weewx-aprx',
-            description='Emit a aprs wxnow.txt for LOOP data.',
+            description='Emit a aprs aprx_wx.txt for LOOP data.',
             author="Mohd Misnan",
             author_email="9m2tpt@gmail.com",
             process_services='user.weewx-aprx',
             config={
-                'CumulusWXNow' : {
-                    'filename': '/var/tmp/wxnow.txt'}},
+                'weewx-aprx' : {
+                    'filename': '/var/tmp/aprx_wx.txt'}},
             files=[('bin/user', ['bin/user/aprx.py'])]
             )
